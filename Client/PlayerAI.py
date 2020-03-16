dee349154389e04e59f832139618e32188e57722
DantElemer <farkasdomonkosl@gmail.com>
2020-03-16 21:42:53 +0100

    Polish

--- Client/PlayerAI.py
+++ Client/PlayerAI.py
@@ -1,7 +1,7 @@
-import pygame as pg
 import PlayerLogic
 from enum import Enum
 import random
+import Player
 
 
 class Movement(Enum):
@@ -10,21 +10,13 @@ class Movement(Enum):
     IDLE = "IDLE"
 
 
-class PlayerAI(pg.sprite.Sprite):
+class PlayerAI(Player.Player):
     def __init__(self, player_id):
-        super(PlayerAI, self).__init__()
-        self.surf = pg.image.load("../img/apple_test_image.png").convert()
-        self.surf.set_colorkey((255, 253, 201), pg.RLEACCEL)
-        self.rect = self.surf.get_rect()
-        self._id = player_id
+        super(PlayerAI, self).__init__(player_id)
         self._dir = Movement.IDLE
 
     def update(self, pressed_keys, events):
-        if random.random() < 0.1:
+        if random.random() < 0.1:  # so that it won't shake too much
             self._dir = random.choice([m.value for m in Movement])
         network_messages = [self._dir]
         PlayerLogic.players[self._id].process_requests(network_messages, self)
-
-    def pos_update(self, x, y):
-        # TODO shall be called from update, messages loaded from networking by id
-        self.rect.center = (x, y)
--- Server/PlayerLogic.py
+++ Server/PlayerLogic.py
@@ -1,13 +1,13 @@
 class PlayerLogic:
     X_MIN = 0
-    X_MAX = 800  # todo AUTOMATICALLY GET SCREEN SIZES
+    X_MAX = 800  # TODO use screen sizes
     Y_MIN = 0
     Y_MAX = 600
 
     def __init__(self, player_id):
         self._id = player_id
         self._speed = 5
-        self._x = 100+player_id*100
+        self._x = 100+player_id*100  # for testing
         self._y = 100+player_id*100
 
     def process_requests(self, network_messages, player):
@@ -18,13 +18,14 @@ class PlayerLogic:
             elif mess == "RIGHT":
                 self._x = self._x + self._speed
 
+        # keep the player in screen
         self._x = min(max(self._x, self.X_MIN), self.X_MAX)
         self._y = min(max(self._y, self.Y_MIN), self.Y_MAX)
 
-        player.pos_update(self._x, self._y)
+        player.pos_update(self._x, self._y)  # TODO again it's a network imitation here...
 
     def update(self, pressed_keys, events):
-        pass
+        pass  # TODO load messages, and call process_requests...
 
 
 # FOR testing
