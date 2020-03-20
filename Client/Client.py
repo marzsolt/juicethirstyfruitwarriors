926f0a60a7250a53f9d9ea0661a82e1963f3e906
molnarpetra <57253701+molnarpetra@users.noreply.github.com>
2020-03-20 21:16:48 +0100

    More polishing

--- Client/Client.py
+++ Client/Client.py
@@ -19,23 +19,21 @@ class Client:
         else:
             host = socket.gethostname()  # or 'IP address'
             port = 12145  # Random port number
-            self.communicator = ClientCommunicator.ClientCommunicator(self, host, port)
+            self.__communicator = ClientCommunicator.ClientCommunicator(self, host, port)
 
-            self.communicator.start()
+            self.__communicator.start()
 
     def receive_message(self, message):
         print("Server sent: ", message)
         message_split = message.split(";")
         if message_split[0] == "kakao":
             print("I sent: csoki")
-            self.communicator.send_message("csoki")
+            self.send_message("csoki")
 
     def send_message(self, message):
-        self.communicator.send_message(message)
+        self.__communicator.send_message(message)
 
-    def run(self):
-        while True:
-            pass
+
 
 
 client = Client()
--- Server/Server.py
+++ Server/Server.py
@@ -19,57 +19,51 @@ class Server(threading.Thread):
             raise Exception("This class is a singleton!")
         else:
             threading.Thread.__init__(self)
-            self.port_number = 12145
-            self.host = socket.gethostname()
-            self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.serverSocket.bind((self.host, self.port_number))
-            self.serverSocket.listen(5)
+            self.__port_number = 12145
+            self.__host = socket.gethostname()
+            self.__serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.__serverSocket.bind((self.__host, self.__port_number))
+            self.__serverSocket.listen(5)
             self.id_counter = 0
 
-            self.serverCommunicatorsList = []
-            self.IDs = []
-
-    def close(self):
-        self.s.close()
+            self.__serverCommunicatorsList = []
 
     def receive_message(self, message, ID):
         print(f"Client {ID} sent:", message)
         message_split = message.split(";")
         if message_split[0] == "csoki":
-            self.serverCommunicatorsList[ID].send_message("koszonom")
+            self.send_message("koszonom", ID)
 
-    def get_communicator_from_id(self, ID):
-        for communicator in self.serverCommunicatorsList:
+    def __get_communicator_from_id(self, ID):
+        for communicator in self.__serverCommunicatorsList:
             if communicator.ID == ID:
                 return communicator
-        raise ValueError("Unexpected value of ID")
+        raise ValueError("Invalid value of ID")
 
     def send_message(self, message, ID):
-        communicator = self.get_communicator_from_id(ID)
-        communicator.send(message)
+        communicator = self.__get_communicator_from_id(ID)
+        communicator.send_message(message)
 
     def send_all(self, message):
-        for communicators in self.serverCommunicatorsList:
+        for communicators in self.__serverCommunicatorsList:
             communicators.send(message)
 
-    def new_client(self, _new_client):
+    def __new_client(self, _new_client):
         newCom = ServerCommunicator.ServerCommunicator(_server=self, _client=_new_client, ID=self.id_counter)
         newCom.start()
-        self.serverCommunicatorsList.append(newCom)
-        self.IDs.append(self.id_counter)
-
-        newCom.send_message("kakao")
+        self.__serverCommunicatorsList.append(newCom)
+        self.send_message("kakao", newCom.ID)
 
         self.id_counter = self.id_counter + 1
 
-    def accept_clients(self):
-        new_client, addr = self.serverSocket.accept()
+    def __accept_clients(self):
+        new_client, addr = self.__serverSocket.accept()
         print("got connection")
-        self.new_client(new_client)
+        self.__new_client(new_client)
 
     def run(self):
         while True:
-            self.accept_clients()
+            self.__accept_clients()
 
 
 Server()
