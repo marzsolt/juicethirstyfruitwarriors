aabfc6a7ef89c1ef9100e0b33cc1732cc840e288
marzsolt <markos.zsolt.25@gmail.com>
2020-04-24 17:41:19 +0300

    Timer polish

--- Server/__main__.py
+++ Server/__main__.py
@@ -5,15 +5,8 @@ from juicethirstyfruitwarriors.Server.Server import Server
 from juicethirstyfruitwarriors.Server.Game import Game
 from juicethirstyfruitwarriors.Server.Timer import Timer
 
-def printer(every_s):
-    print("I am being printed rougly every " + str(every_s) + " s")
-    Timer.sch_fun(40*every_s, printer, (every_s,))
-    
-def hello_timer():
-    print("Hello timer! :)")
 
-
-def main():
+def main():            
     def update():
         s.enter(1.0/fps, 1, update, ())  # call update again in 25 ms
         Timer.update()
@@ -25,10 +18,6 @@ def main():
 
     # Game
     game = Game()
-
-    Timer.sch_fun(40*5, printer, (5,))
-    Timer.sch_fun(40*2, printer, (2,))
-    Timer.sch_fun(40*3, hello_timer, ())
 
     # Setup regular update "loop"
     s = sched.scheduler(time.time, time.sleep)
