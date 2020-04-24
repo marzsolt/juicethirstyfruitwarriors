48e632a6f01da683d3af245b4802d8387bef4247
marzsolt <markos.zsolt.25@gmail.com>
2020-04-24 17:26:10 +0300

    Timer

--- Server/Timer.py
+++ Server/Timer.py
@@ -1,5 +1,19 @@
 class Timer:
+    sch_list = []
     
     @staticmethod
     def sch_fun(delay, fun, arg):
-        print("asd")
+        Timer.sch_list.append((delay, fun, arg))
+        
+    @staticmethod
+    def update():
+        expired = []
+        for i, sch in enumerate(Timer.sch_list):
+            if sch[0] == 0:
+                sch[1](*sch[2])
+                expired.append(i)
+
+        Timer.sch_list = [
+            (sch[0] - 1, sch[1], sch[2])
+            for i, sch in enumerate(Timer.sch_list) if i not in expired
+        ]
--- Server/__main__.py
+++ Server/__main__.py
@@ -3,11 +3,20 @@ import time
 
 from juicethirstyfruitwarriors.Server.Server import Server
 from juicethirstyfruitwarriors.Server.Game import Game
+from juicethirstyfruitwarriors.Server.Timer import Timer
+
+def printer(every_s):
+    print("I am being printed rougly every " + str(every_s) + " s")
+    Timer.sch_fun(40*every_s, printer, (every_s,))
+    
+def hello_timer():
+    print("Hello timer! :)")
 
 
 def main():
     def update():
         s.enter(1.0/fps, 1, update, ())  # call update again in 25 ms
+        Timer.update()
         game.update()
 
     # Networking
@@ -16,6 +25,10 @@ def main():
 
     # Game
     game = Game()
+
+    Timer.sch_fun(40*5, printer, (5,))
+    Timer.sch_fun(40*2, printer, (2,))
+    Timer.sch_fun(40*3, hello_timer, ())
 
     # Setup regular update "loop"
     s = sched.scheduler(time.time, time.sleep)
