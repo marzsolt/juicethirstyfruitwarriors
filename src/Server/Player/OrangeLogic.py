d94550d11ad93d42d403dc68ed0edaac04e6212c
marzsolt <markos.zsolt.25@gmail.com>
2020-04-29 22:26:37 +0300

    Revert reverted commit.

--- src/Server/Player/OrangeLogic.py
+++ src/Server/Player/OrangeLogic.py
@@ -6,7 +6,7 @@ import src.Server.Network_communication.server_message_constants as sermess
 import src.Client.Network_communication.client_message_constants as climess
 
 from src.utils.BaseMessage import BaseMessage
-
+from src.utils.Timer import Timer
 
 
 class OrangeLogic(PlayerLogic):
@@ -16,25 +16,23 @@ class OrangeLogic(PlayerLogic):
         self._is_attacking = False
         self._attack_strength = 5
 
-
     def _process_requests(self, network_messages):
         super()._process_requests(network_messages)
         for mess in network_messages:
-            if mess.type == climess.MessageType.ORANGE_ATTACK:
-                if self._vel != Vector2D.zero():
-                    self._attack()
-
-                    #  ACK valid attack -> actually for rolling purposes
-                    mes = BaseMessage(sermess.MessageType.ORANGE_ROLL, sermess.Target.ORANGE_PLAYER + str(self._id))
-                    Server.get_instance().send_all(mes)
+            if mess.type == climess.MessageType.ORANGE_ATTACK and self._can_attack and self._vel != Vector2D.zero():
+                self._attack()
+                #  ACK valid attack -> actually for rolling purposes
+                mes = BaseMessage(sermess.MessageType.ORANGE_ROLL, sermess.Target.ORANGE_PLAYER + str(self._id))
+                Server.get_instance().send_all(mes)
 
     def _attack(self):
-        if super()._attack() and not self._is_flying:
+        if self._can_attack and not self._is_flying:
+            super()._attack()
             self._is_attacking = True
             self._add_ground_directed_force(self._attack_strength, self.my_dir())
             self._impact()   # TODO LIAR!
-            return True
-        return False
+
+
 
     def _impact(self):
         if self._is_attacking:
