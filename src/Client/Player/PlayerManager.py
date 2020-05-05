dcb3c1864a6a848a6ea6d7a2ff8f17639dd62ddd
DantElemer <farkasdomonkosl@gmail.com>
2020-05-05 13:11:31 +0200

    Client AIs removed - problem solved

--- src/Client/Player/PlayerManager.py
+++ src/Client/Player/PlayerManager.py
@@ -1,6 +1,4 @@
 from src.Client.Network_communication.Client import Client
-from src.Client.Player.Player import Player, PicFile
-from src.Client.Player.PlayerAI import PlayerAI
 from src.Client.Player.ApplePlayer import ApplePlayer
 from src.Client.Player.OrangePlayer import OrangePlayer
 
@@ -30,9 +28,9 @@ class PlayerManager:
         for player_id in orange_human_ids:
             self.players.append(OrangePlayer(player_id))
         for player_id in apple_ai_ids:
-            self.players.append(PlayerAI(player_id, PicFile.APPLE))
+            self.players.append(ApplePlayer(player_id))
         for player_id in orange_ai_ids:
-            self.players.append(PlayerAI(player_id, PicFile.ORANGE))
+            self.players.append(OrangePlayer(player_id))
 
     def update(self, pressed_keys, events):
         """ Call every player's update. Delegate events to own player."""
